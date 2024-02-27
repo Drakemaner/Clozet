e05bd4a9294a8f8621d77216ba5c3d8d393a59db
Jenner <guilhermejenner10@gmail.com>
2024-02-27 17:18:55 -0300

    Adição do Componente Footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HeaderComponent} from "./header/header.component";
 import {IonicModule} from "@ionic/angular";
+import {FooterComponent} from "./footer/footer.component";
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
