9a8315e7db10081e3be79d8de3c1d3a2064e9914
Jenner <guilhermejenner10@gmail.com>
2024-03-04 10:58:08 -0300

    Ajustar Funcionalidade nos botões Footer e Header

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Roupas } from 'src/app/pseudoBanco/roupas';
-import { CameraService } from 'src/app/services/camera/camera.service';
-import { ActionSheetController } from '@ionic/angular';
-import { Router } from '@angular/router';
-import { StorageService } from 'src/app/services/storage/storage.service';
-import { Platform } from '@ionic/angular';
+import { Component } from '@angular/core';
+
+
 
 
 @Component({
@@ -14,48 +10,9 @@ import { Platform } from '@ionic/angular';
 })
 export class FooterComponent   {
 
-  tipoRoupa = ''
-  roupas = Roupas
 
-  constructor(private platform : Platform, private cameraService : CameraService, private storageService : StorageService , private actionSheet : ActionSheetController, private router : Router) { }
+  constructor() { }
 
   
-
-  showActions = async () => {
-    const result = await this.actionSheet.create({
-      header: 'Tipo da Roupa',
-      buttons: [
-        {
-          text: 'Head',
-          handler: () => this.tirarFoto('head')
-        },
-        {
-          text: 'Tee',
-          handler: () => this.tirarFoto('tee')
-        },
-        {
-          text: 'Calça',
-          handler: () => this.tirarFoto('pants')  
-        },
-        {
-          text: 'Tênis',
-          handler: () => this.tirarFoto('shoes')
-        },
-        {
-          text: 'Cancelar',
-          role: 'cancel'
-        }
-      ],
-    })
-    
-    await result.present();
-  }
-
-  tirarFoto(tipo : string){
-     
-    this.tipoRoupa = tipo
-    
-    this.cameraService.takePicture(this.roupas, this.tipoRoupa)
-  }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CameraService } from 'src/app/services/camera/camera.service';
+import { ActionSheetController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { StorageService } from 'src/app/services/storage/storage.service';
+import { Platform } from '@ionic/angular';
+import { Roupas } from 'src/app/pseudoBanco/roupas';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent  {
 
-  constructor() { }
+  constructor(private platform : Platform, private cameraService : CameraService, private storageService : StorageService , private actionSheet : ActionSheetController, private router : Router) { }
 
-  ngOnInit() {}
+  tipoRoupa = ''
+  roupas = Roupas
+
+  showActions = async () => {
+    const result = await this.actionSheet.create({
+      header: 'Tipo da Roupa',
+      buttons: [
+        {
+          text: 'Head',
+          handler: () => this.tirarFoto('head')
+        },
+        {
+          text: 'Tee',
+          handler: () => this.tirarFoto('tee')
+        },
+        {
+          text: 'Calça',
+          handler: () => this.tirarFoto('pants')  
+        },
+        {
+          text: 'Tênis',
+          handler: () => this.tirarFoto('shoes')
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        }
+      ],
+    })
+    
+    await result.present();
+  }
+
+  tirarFoto(tipo : string){
+     
+    this.tipoRoupa = tipo
+    
+    this.cameraService.takePicture(this.roupas, this.tipoRoupa)
+  }
 
 }
