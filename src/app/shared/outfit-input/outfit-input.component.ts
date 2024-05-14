2ba7f3c4406fe15cd4adf485643b579e41652a41
Drakemaner <guilhermejenner10@gmail.com>
2024-05-14 14:23:10 -0300

    Correção de Salvar Outfit pela Home Direto

--- src/app/shared/outfit-input/outfit-input.component.ts
+++ src/app/shared/outfit-input/outfit-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Roupas } from 'src/app/Roupas/roupas';
@@ -14,15 +14,22 @@ export class OutfitInputComponent {
   inputName = ''
   outfit = Outfit
   roupas = Roupas
+
+  @Input()
+  type : string = 'New'
+
   @Output()
   Show : EventEmitter<boolean> = new EventEmitter()
 
   constructor(private router : Router) { }
 
   criarOutfit(){
-    this.roupas.forEach(a=> {
-      a.display == 'display: flex' ? a.display = 'display: none' : null
-    })
+    if(this.type == 'New'){
+      this.roupas.forEach(a=> {
+        a.display == 'display: flex' ? a.display = 'display: none' : null
+      })
+    }
+    
     this.outfit.nome = this.inputName
     this.Show.emit(false)
     this.router.navigate(['/home'])
